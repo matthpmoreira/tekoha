a04d971fe231a4e4b04b2799f40b838a01db9a58
Matheus Henrique <matthpmoreira@gmail.com>
2024-07-26 16:23:39 -0300

    refactor: improve script readability and security

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-const address = "***REMOVED***";
-// const address = "mc.hypixel.net"; // For testing when reloading frequently
-
-fixTextOnWindows();
+fixTextOnWindows(); // For pre-existing .widget elements
 main();
-fixTextOnWindows();
-
-const statusIcon = document.querySelector(".status_icon img");
-const statusText = document.querySelector(".status_text");
-const players = document.querySelector(".players");
-const playersText = document.querySelector(".players_text");
-const playersGrid = document.querySelector(".players_grid");
 
 async function main() {
+    const address = "***REMOVED***";
     const response = await fetch("https://api.mcsrvstat.us/3/" + encodeURIComponent(address));
     const data = await response.json();
+    const status = {
+        icon: document.querySelector(".status_icon img"),
+        text: document.querySelector(".status_text")
+    };
 
     console.log(data);
 
     if (data.online) {
-        statusText.textContent = "O servidor está aberto!";
-        statusIcon.src = "assets/icons/open-lock.png";
+        status.text.textContent = "O servidor está aberto!";
+        status.icon.src = "assets/icons/open-lock.png";
         setPlayerList(data.players);
     } else {
         statusText.textContent = "O servidor está fechado!";
     }
 }
 
-function setPlayerList(playerData) {
-    players.style.display = "flex";
+function setPlayerList({ list }) {
+    const players = {
+        element: document.querySelector(".players"),
+        text: document.querySelector(".players_text"),
+        grid: document.querySelector(".players_grid")
+    };
 
-    if (playerData.online === 0)
-        return;
+    players.element.style.display = "flex";
+    if (!list) return;
+    players.text.textContent = "Jogadores online";
 
-    playersText.textContent = "Jogadores online";
-
-    for (const p of playerData.list)
-        playersGrid.append(generatePlayerEntry(p.name));
+    for (const player of list)
+        players.grid.append(generatePlayerEntry(player.name));
 }
 
 function generatePlayerEntry(name) {
@@ -46,7 +44,7 @@ function generatePlayerEntry(name) {
 
     const nameDiv = document.createElement("div");
     nameDiv.classList.add("players_name");
-    nameDiv.textContent = `${name}`;
+    nameDiv.textContent = name;
 
     const entry = document.createElement("div");
     entry.classList.add("players_entry", "widget", "-disabled");
@@ -58,10 +56,11 @@ function generatePlayerEntry(name) {
 // Windows renders text severe pixels above center, so this fixes it
 // Looks normal on Android and Linux, unknown on iOS and macOS
 function fixTextOnWindows() {
-    if (navigator.userAgent.match(/Windows/)) {
-        for (const button of document.querySelectorAll(".widget:not(.-icon)")) {
-            button.style.paddingTop = "0.7em";
-            button.style.paddingBottom = "0";
-        }
+    if (!navigator.userAgent.match(/Windows/))
+        return;
+
+    for (const button of document.querySelectorAll(".widget:not(.-icon)")) {
+        button.style.paddingTop = "0.7em";
+        button.style.paddingBottom = "0";
     }
 }
